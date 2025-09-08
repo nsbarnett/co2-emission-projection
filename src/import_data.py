d85dff6f1d1925855e245d2c95ec867685ceda6b
Naim Barnett <naim.barnett@gmail.com>
2025-09-08 17:57:56 -0500

    Initial data quality check and analysis

---
+++ src/import_data.py
@@ -0,0 +1,14 @@
+# Data manipulation and analysis
+import pandas as pd
+import numpy as np
+
+# Data visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Suppress warnings for cleaner output
+import warnings
+warnings.filterwarnings("ignore")
+
+# Load the Dataset
+dataframe = pd.read_csv("../data/archive/co2_emissions_kt_by_country.csv")
