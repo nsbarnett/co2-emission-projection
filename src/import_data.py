6d2d39a1cc89af364d3a0bf9d1513361afd7f588
Naim Barnett <naim.barnett@gmail.com>
2025-09-08 18:12:20 -0500

    mypy fix for CI run failure

--- src/import_data.py
+++ src/import_data.py
@@ -1,11 +1,5 @@
 # Data manipulation and analysis
 import pandas as pd
 
-# Data visualization
-
-# Suppress warnings for cleaner output
-import warnings
-warnings.filterwarnings("ignore")
-
 # Load the Dataset
 dataframe = pd.read_csv("../data/archive/co2_emissions_kt_by_country.csv")
